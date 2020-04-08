85c24241dc3834caf8dd723e46aa373d2c9b5480
Warren Harper II <warrenharper2@att.net>
2020-04-08 00:30:51 +0000

    Automatically backed up by Learn

--- db/migrate/20200407195850_create_passenger_taxis.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinTableRides < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :passenger_taxis do |t|
-      t.integer :passenger_id
-      t.integer :taxi_id
-      t.timestamps null: false
-    end
-  end
-end
